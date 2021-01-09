f67ae09e07afeb26479bf06c197c420608cad340
xzgang0499 <xzgnag0499@qq.com>
2021-01-09 22:16:03 +0800

    初始化

---
+++ src/main/java/com/xzg/wlxx/WlxxApplication.java
@@ -0,0 +1,13 @@
+package com.xzg.wlxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WlxxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WlxxApplication.class, args);
+	}
+
+}
