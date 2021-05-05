71a7c88a04b2fc897c51aa6250be5bc88ac6bf69
xzgang0499 <xzgnag0499@qq.com>
2021-05-05 21:36:38 +0800

    解决多模块相互编译依赖问题

---
+++ wlxx-framework/src/main/java/com/xzg/wlxx/framework/ctrl/ExceptionCtrl.java
@@ -0,0 +1,29 @@
+package com.xzg.wlxx.framework.ctrl;
+
+
+import com.xzg.wlxx.framework.model.AjaxResult;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Author: 肖志刚
+ * @Date: 2020/7/5 22:00
+ */
+@ControllerAdvice
+public class ExceptionCtrl extends BaseCtrl {
+    /**
+     * @param req
+     * @param e
+     * @return
+     * @throws Exception
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public AjaxResult defaultError(HttpServletRequest req, HttpServletResponse res, Exception e) throws Exception{
+        return error(e);
+    }
+}
