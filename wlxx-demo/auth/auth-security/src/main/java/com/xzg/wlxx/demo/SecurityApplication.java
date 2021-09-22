d7635cfeb6d8e90357c520a25e1c1de285ce91b2
xzgang0499 <xzgang0499@qq.com>
2021-09-22 18:09:05 +0800

    读取application.yml中的端口，优化中

--- wlxx-demo/auth/auth-security/src/main/java/com/xzg/wlxx/demo/SecurityApplication.java
+++ wlxx-demo/auth/auth-security/src/main/java/com/xzg/wlxx/demo/SecurityApplication.java
@@ -2,11 +2,12 @@ package com.xzg.wlxx.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author edas
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SecurityApplication {
 
     public static void main(String[] args) {
