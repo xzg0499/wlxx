0d5d761263e85e6bc1a4c89b090bc633f4e2d92f
xzgang0499 <xzgang0499@qq.com>
2021-11-04 17:49:12 +0800

    实现几种设计模式，framework改名为common，对其的依赖方式变更

--- wlxx-user/src/main/java/com/xzg/wlxx/user/ctrl/UserCtrl.java
+++ wlxx-user/src/main/java/com/xzg/wlxx/user/ctrl/UserCtrl.java
@@ -1,23 +1,18 @@
 package com.xzg.wlxx.user.ctrl;
 
-import com.xzg.wlxx.framework.ctrl.BaseCtrl;
+
+import com.xzg.wlxx.common.ctrl.BaseCtrl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Created by IntelliJ IDEA.
- * User: xzgang
- * Date: 2021/4/30
+ * @Author: 肖志刚
+ * @Date: 2021/11/4 17:15
  */
-
 @RestController
 public class UserCtrl extends BaseCtrl {
-//    @GetMapping("/hello-user")
-//    public AjaxResult sayHello(){
-//        return success("Hello User!");
-//    }
-//    @GetMapping("/test-error")
-//    public AjaxResult testError(){
-//        return error();
-//    }
+    @GetMapping("/hello-user")
+    public String hello(){
+        return "Hello User!";
+    }
 }
