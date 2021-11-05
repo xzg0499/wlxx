a8f97e47001fe074e11a2196884e2124bfaa43e7
xzgang0499 <xzgang0499@qq.com>
2021-11-05 17:07:27 +0800

    docker-compose编译nacos-server/mysql/redis/rabbitmq，nacos针对mysql进行了优化

--- wlxx-user/src/main/java/com/xzg/wlxx/user/UserApplication.java
+++ wlxx-user/src/main/java/com/xzg/wlxx/user/UserApplication.java
@@ -3,12 +3,14 @@ package com.xzg.wlxx.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Author: 肖志刚
  * @Date: 2021/11/4 17:01
  */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class);
