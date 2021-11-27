b3d435668fb74f452725122b6d12180c3ebfa593
xzgang0499 <xzgang0499@qq.com>
2021-11-27 16:02:42 +0800

    重新整理docker，增加config.sql文件

--- script/user.sql
+++ script/user.sql
@@ -1,14 +1,28 @@
-use user;
+use db_user;
 
-create table t_employee(
-    id varchar(64) primary key,
-    emp_name varchar(128),
-)
-
+-- 用户表
+drop table if exists t_user;
 create table t_user(
-    id varchar(64) primary key,
-    username varchar(128),
-    password varchar(128),
+    id varchar(60) primary key comment '主键',
+    creator varchar(60) comment '创建人',
+    create_time timestamp not null default current_timestamp() comment '创建时间',
+    last_updater varchar(60) comment '最后一次更新人',
+    last_update_time timestamp not null default current_timestamp() comment '最后一次更新时间',
 
-    sex int,
-)
+    username varchar(100) comment '用户名',
+    loginname varchar(100) comment '用户账号',
+    password varchar(100) comment '密码',
+
+);
+
+-- 人员表（员工表）
+drop table if exists t_person(
+    id varchar(60) primary key comment '主键',
+    creator varchar(60) comment '创建人',
+    create_time timestamp not null default current_timestamp() comment '创建时间',
+    last_updater varchar(60) comment '最后一次更新人',
+    last_update_time timestamp not null default current_timestamp() comment '最后一次更新时间',
+
+    name varchar(60) comment '姓名',
+    sex tinyint(1) comment '性别',
+);
