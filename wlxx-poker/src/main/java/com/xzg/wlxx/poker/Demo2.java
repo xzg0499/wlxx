0cb2a0177529953206fb79927bdecf3fde406bac
xzgang0499 <xzgang0499@qq.com>
2022-05-07 09:15:10 +0800

    迁移

---
+++ wlxx-poker/src/main/java/com/xzg/wlxx/poker/Demo2.java
@@ -0,0 +1,17 @@
+package com.xzg.wlxx.poker;
+
+/**
+ * @author xzgang0499
+ * @date 2022-05-05
+ * @since jdk1.8
+ */
+public class Demo2 {
+
+    public static void main(String[] args) throws Exception{
+
+    }
+
+    public void handleCardType() throws Exception{
+
+    }
+}
