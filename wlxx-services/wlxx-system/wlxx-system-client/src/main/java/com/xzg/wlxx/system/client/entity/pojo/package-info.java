e89850a19cc00b5bb15b78c8a382e95249da0dfd
xzgang0499 <xzgang0499@qq.com>
2022-09-13 21:29:44 +0800

    调整

--- wlxx-services/wlxx-system/wlxx-system-client/src/main/java/com/xzg/wlxx/system/client/entity/pojo/package-info.java
+++ wlxx-services/wlxx-system/wlxx-system-client/src/main/java/com/xzg/wlxx/system/client/entity/pojo/package-info.java
@@ -1,5 +1,6 @@
 /**
- * （Plain Ordinary Java Object）简单的 Java 对象，实际就是普通的 JavaBeans，是为了避免和 EJB（Enterprise JavaBean）混淆所创造的简称。
+ * Plain Ordinary Java Object 简单的 Java 对象，实际就是普通的 JavaBeans，是为了避免和 EJB（Enterprise JavaBean）混淆所创造的简称。
+ *
  * @author xzgan
  * @since 2022/9/13
  */
