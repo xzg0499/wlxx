a55574e744eed3076a7185a465cf639d64311855
xzgang0499 <xzgang0499@qq.com>
2022-11-08 18:22:27 +0800

    feat(配置): 移除bom模块

--- wlxx-services/wlxx-system/wlxx-system-provider/src/main/java/com/xzg/wlxx/system/SystemApplication.java
+++ wlxx-services/wlxx-system/wlxx-system-provider/src/main/java/com/xzg/wlxx/system/SystemApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 系统管理，启动类
@@ -15,6 +16,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @MapperScan("com.xzg.wlxx.system.mapper")
 @SpringBootApplication
+@ComponentScan(value = {"com.xzg.wlxx.common.core"})
 // @ComponentScan(excludeFilters = @ComponentScan.Filter(
 //         type = FilterType.REGEX, pattern = "com.xzg.wlxx.common.core.base.BaseController"
 // ))
