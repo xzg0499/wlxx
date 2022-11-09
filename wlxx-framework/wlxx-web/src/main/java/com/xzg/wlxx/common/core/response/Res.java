774b9887e02208e5a5cecefae3942d93219510d4
xzgang0499 <xzgang0499@qq.com>
2022-11-09 15:44:46 +0800

    feat(配置): 重写

--- wlxx-framework/wlxx-web/src/main/java/com/xzg/wlxx/common/core/response/Res.java
+++ wlxx-framework/wlxx-web/src/main/java/com/xzg/wlxx/common/core/response/Res.java
@@ -1,6 +1,8 @@
 package com.xzg.wlxx.common.core.response;
 
 import com.xzg.wlxx.common.core.enums.ResEnum;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,19 +18,24 @@ import java.io.Serializable;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel("接口出参")
 public class Res<T> implements Serializable {
+    @ApiModelProperty("响应编码")
     private Integer code;
+    @ApiModelProperty("消息")
     private String msg;
+    @ApiModelProperty("接口是否成功")
     private Boolean success;
+    @ApiModelProperty("出参数据")
     private T data;
 
-    public Res(Integer code, String msg, T data){
+    public Res(Integer code, String msg, T data) {
         this.code = code;
         this.msg = msg;
         this.data = data;
     }
 
-    public Res(ResEnum resEnum){
+    public Res(ResEnum resEnum) {
         this.code = resEnum.getCode();
         this.msg = resEnum.getMsg();
     }
