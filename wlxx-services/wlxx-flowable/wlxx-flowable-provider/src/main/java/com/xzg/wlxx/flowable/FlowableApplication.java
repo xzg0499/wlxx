b22ea549297f438581693e7f988ab12b02209729
xzgang0499 <xzgang0499@qq.com>
2022-11-22 16:10:35 +0800

    feat(功能): flowable-ui启动，并注册服务V1

--- wlxx-services/wlxx-flowable/wlxx-flowable-provider/src/main/java/com/xzg/wlxx/flowable/FlowableApplication.java
+++ wlxx-services/wlxx-flowable/wlxx-flowable-provider/src/main/java/com/xzg/wlxx/flowable/FlowableApplication.java
@@ -22,4 +22,22 @@ public class FlowableApplication extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(FlowableApplication.class);
     }
+
+    //@Bean
+    //public CommandLineRunner init(final RepositoryService repositoryService,
+    //                              final RuntimeService runtimeService,
+    //                              final TaskService taskService) {
+    //
+    //    return new CommandLineRunner() {
+    //        @Override
+    //        public void run(String... strings) throws Exception {
+    //            System.out.println("Number of process definitions : "
+    //                    + repositoryService.createProcessDefinitionQuery().count());
+    //            System.out.println("Number of tasks : " + taskService.createTaskQuery().count());
+    //            runtimeService.startProcessInstanceByKey("oneTaskProcess");
+    //            System.out.println("Number of tasks after process start: "
+    //                    + taskService.createTaskQuery().count());
+    //        }
+    //    };
+    //}
 }
---
+++ wlxx-services/wlxx-flowable/wlxx-flowable-provider/src/main/java/com/xzg/wlxx/flowable/controller/DmnController.java
@@ -0,0 +1,10 @@
+package com.xzg.wlxx.flowable.controller;
+
+/**
+ * @author xzgan
+ * @project wlxx
+ * @package com.xzg.wlxx.flowable.controller
+ * @date 2022/11/22 11:50
+ */
+public class DmnController {
+}
