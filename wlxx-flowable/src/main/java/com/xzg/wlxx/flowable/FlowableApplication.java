65242b954ef4da5cc34b2448ccf6d5355792a564
xzgang0499 <xzgang0499@qq.com>
2023-08-07 11:34:58 +0800

    add flowable jar

--- wlxx-flowable/src/main/java/com/xzg/wlxx/flowable/FlowableApplication.java
+++ wlxx-flowable/src/main/java/com/xzg/wlxx/flowable/FlowableApplication.java
@@ -1,10 +1,21 @@
 package com.xzg.wlxx.flowable;
 
+import org.flowable.ui.admin.repository.ServerConfigRepository;
+import org.flowable.ui.admin.service.engine.FlowableClientService;
+import org.flowable.ui.common.repository.UuidIdGenerator;
+import org.flowable.ui.common.tenant.TenantProvider;
+import org.flowable.ui.modeler.repository.ModelHistoryRepository;
+import org.flowable.ui.modeler.repository.ModelRelationRepository;
+import org.flowable.ui.modeler.repository.ModelRepository;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanNameGenerator;
+import org.springframework.beans.factory.support.DefaultBeanNameGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 /**
  * @author xzgan
@@ -12,17 +23,24 @@ import org.springframework.context.annotation.ComponentScan;
  * @package com.xzg.wlxx.flowable
  * @date 2022/11/17 15:37
  */
-@SpringBootApplication
-@ComponentScan(basePackages = {"org.flowable.engine"})
+@SpringBootApplication(proxyBeanMethods = false)
+@ComponentScan(nameGenerator = DefaultBeanNameGenerator.class, basePackages = {"org.flowable"}
+        , excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {org.flowable.ui.idm.servlet.AppDispatcherServletConfiguration.class
+        , org.flowable.ui.modeler.servlet.AppDispatcherServletConfiguration.class, org.flowable.ui.task.servlet.AppDispatcherServletConfiguration.class
+        , FlowableClientService.class, ServerConfigRepository.class, UuidIdGenerator.class, ModelRepository.class, TenantProvider.class
+        , ModelHistoryRepository.class, ModelRelationRepository.class
+}))
 public class FlowableApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(FlowableApplication.class, args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(FlowableApplication.class);
+    public static class SpringBeanNameGenerator implements BeanNameGenerator {
+        @Override
+        public String generateBeanName(BeanDefinition definition, BeanDefinitionRegistry registry) {
+            return null;
+        }
     }
 
     //@Bean
---
+++ wlxx-flowable/src/main/java/com/xzg/wlxx/flowable/config/BeanNameGenerator.java
@@ -0,0 +1,17 @@
+//package com.xzg.wlxx.flowable.config;
+//
+//import org.springframework.beans.factory.config.BeanDefinition;
+//import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+//import org.springframework.context.annotation.AnnotationBeanNameGenerator;
+//
+///**
+// * @author xzgan
+// * @date 2023/8/1
+// */
+//public class BeanNameGenerator extends AnnotationBeanNameGenerator {
+//
+//    @Override
+//    public String generateBeanName(BeanDefinition definition, BeanDefinitionRegistry registry) {
+//        return definition.getBeanClassName();
+//    }
+//}
