3bfd0b2b8d4a077973286c089bc2b02730b89c40
xzgang0499 <xzgang0499@qq.com>
2023-09-21 15:30:20 +0800

    引入gateway

---
+++ wlxx-gateway/build.gradle.kts
@@ -0,0 +1,23 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+plugins {
+    id("wlxx.application")
+}
+
+//ext["springCloudAlibabaVersion"] = "2022.0.0.0-RC2"
+ext["springCloudAlibabaVersion"] = "2022.0.0.0"
+
+tasks.withType<BootJar> {
+    manifest {
+        attributes["Main-Class"] = "org.springframework.boot.loader.JarLauncher"
+        attributes["Start-Class"] = "com.xzg.wlxx.system.SystemApplication"
+    }
+}
+
+dependencies {
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation("com.github.xiaoymin:knife4j-gateway-spring-boot-starter:4.3.0")
+    implementation("com.alibaba.cloud:spring-cloud-starter-alibaba-nacos-discovery:2022.0.0.0")
+//    implementation("com.alibaba.cloud:spring-cloud-starter-alibaba-nacos-config:2022.0.0.0")
+}
+
---
+++ wlxx-gateway/src/main/java/com/xzg/wlxx/gateway/config/Knife4jConfiguration.java
@@ -0,0 +1,7 @@
+package com.xzg.wlxx.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Knife4jConfiguration {
+}
