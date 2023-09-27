6ea146b8485efbcff640b3bc5d65e731e90b2bb4
xzgang0499 <xzgang0499@qq.com>
2023-09-27 14:34:08 +0800

    清理空文件夹task

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,26 @@ configure(allprojects) {
     version = "0.0.1"
 }
 
+tasks.register<DefaultTask>("cleanEmptyFolder") {
+    val file = rootDir
+    val list = mutableListOf<File>();
+    file.walkTopDown()
+        .filter { f -> f.isDirectory }
+        .filter { f -> !f.path.replace(file.path, "").startsWith("\\.") }
+        .filter { it.name.startsWith("wlxx-") }
+        .forEach { f ->
+            var isEmpty = false;
+            f.walkTopDown()
+                .filter { !it.path.contains("build") }
+                .forEach {
+                    isEmpty = it.listFiles()?.isEmpty() == true
+                }
+            if (isEmpty) {
+                list.add(f)
+            }
+        }
+    list.forEach {
+        println(it.name)
+        it.deleteRecursively()
+    }
+}
