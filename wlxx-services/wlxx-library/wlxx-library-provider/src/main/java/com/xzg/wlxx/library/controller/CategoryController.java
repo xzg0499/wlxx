18e307bca38c9daa06322694047bbf95c8e6f321
xzgang0499 <xzgang0499@qq.com>
2023-10-16 16:02:48 +0800

    category导入导出

--- wlxx-services/wlxx-library/wlxx-library-provider/src/main/java/com/xzg/wlxx/library/controller/CategoryController.java
+++ wlxx-services/wlxx-library/wlxx-library-provider/src/main/java/com/xzg/wlxx/library/controller/CategoryController.java
@@ -5,13 +5,14 @@ import com.xzg.wlxx.common.base.ApiResult;
 import com.xzg.wlxx.library.pojo.dto.CategoryDto;
 import com.xzg.wlxx.library.pojo.po.CategoryPo;
 import com.xzg.wlxx.library.service.CategoryService;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -33,5 +34,18 @@ public class CategoryController {
     @PostMapping("page")
     public ApiResult<IPage<CategoryPo>> page(@RequestBody CategoryDto dto) {
         return ApiResult.success(service.search(dto));
+    }
+
+    @PostMapping("import")
+    @Parameter(name = "file")
+    public ApiResult<Boolean> importExcel(@RequestParam("file") MultipartFile file) throws IOException {
+        return ApiResult.success(service.importExcel(file));
+    }
+
+    @PostMapping("export")
+    public ApiResult<Boolean> exportExcel(@RequestBody CategoryDto dto, HttpServletResponse response) throws IOException {
+        // FIXME knife4j导出异常，junit导出正常
+        service.exportExcel(dto, response);
+        return ApiResult.success();
     }
 }
--- wlxx-services/wlxx-library/wlxx-library-provider/src/main/java/com/xzg/wlxx/library/service/CategoryService.java
+++ wlxx-services/wlxx-library/wlxx-library-provider/src/main/java/com/xzg/wlxx/library/service/CategoryService.java
@@ -4,7 +4,10 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.xzg.wlxx.library.pojo.dto.CategoryDto;
 import com.xzg.wlxx.library.pojo.po.CategoryPo;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -15,4 +18,8 @@ public interface CategoryService extends IService<CategoryPo> {
     boolean batchAdd(List<CategoryDto> list);
 
     IPage<CategoryPo> search(CategoryDto dto);
+
+    void exportExcel(CategoryDto dot, HttpServletResponse response) throws IOException;
+
+    boolean importExcel(MultipartFile file) throws IOException;
 }
