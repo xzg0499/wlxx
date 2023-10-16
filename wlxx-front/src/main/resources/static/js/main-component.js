122e7d7a5b1971a059e920e77cb2504135c47da2
xzgang0499 <xzgang0499@qq.com>
2023-10-16 13:42:32 +0800

    优化module

--- wlxx-front/src/main/resources/static/js/main-component.js
+++ wlxx-front/src/main/resources/static/js/main-component.js
@@ -1,12 +1,13 @@
 import { ref } from 'vue'
 export default {
   setup() {
+    const $q = Quasar.useQuasar()
     const name = ref("xzg")
     const age = ref(11)
     const accept = ref(false)
 
     const onSubmit = function() {
-        if (accept !== true) {
+        if (accept.value !== true) {
           $q.notify({
             color: 'red-5',
             textColor: 'white',
@@ -25,11 +26,44 @@ export default {
     }
 
     const onReset =function() {
-        name = null
-        age = null
-        accept = false
+        name.value = null
+        age.value = null
+        accept.value = false
     }
     return { name,age,accept,onSubmit,onReset }
-  }
+  },
+  template: `<q-form
+                         @submit="onSubmit"
+                         @reset="onReset"
+                         class="q-gutter-md"
+                 >
+                     <q-input
+                             filled
+                             v-model="name"
+                             label="Your name *"
+                             hint="Name and surname"
+                             lazy-rules
+                             :rules="[ val => val && val.length > 0 || 'Please type something']"
+                     ></q-input>
+
+                     <q-input
+                             filled
+                             type="number"
+                             v-model="age"
+                             label="Your age *"
+                             lazy-rules
+                             :rules="[
+                       val => val !== null && val !== '' || 'Please type your age',
+                       val => val > 0 && val < 100 || 'Please type a real age'
+                     ]"
+                     ></q-input>
+
+                     <q-toggle v-model="accept" label="I accept the license and terms"></q-toggle>
+
+                     <div>
+                         <q-btn label="Submit" type="submit" color="primary"></q-btn>
+                         <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm"></q-btn>
+                     </div>
+                 </q-form>`
 }
 
