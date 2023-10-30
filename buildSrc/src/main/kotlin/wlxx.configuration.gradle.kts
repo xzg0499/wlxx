29245b05bf473a9d53f4c653be9dfed00fe5b87f
xzgang0499 <xzgang0499@qq.com>
2023-10-30 20:05:58 +0800

    添加maven发布

--- buildSrc/src/main/kotlin/wlxx.configuration.gradle.kts
+++ buildSrc/src/main/kotlin/wlxx.configuration.gradle.kts
@@ -1,3 +1,5 @@
+import gradle.kotlin.dsl.accessors._4a26dd84298dfbdc4ac960278efabc7a.publishing
+
 plugins {
     id("wlxx.dependencies")
 }
@@ -26,7 +28,28 @@ tasks.withType<Test> {
 // FIXME 解决全局build tests无法build
 tasks.build {
     // 全局build跳过test
-    println("test enabled ${tasks.getByName("test").enabled}")
+//    println("test enabled ${tasks.getByName("test").enabled}")
     tasks.getByName("test").enabled = false
-    println("test enabled ${tasks.getByName("test").enabled}")
+//    println("test enabled ${tasks.getByName("test").enabled}")
+}
+
+//gradle.afterProject {
+//    println("after project ===")
+//}
+
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = groupId
+            artifactId = project.name
+            version = version
+
+            from(components["java"])
+        }
+    }
+}
+
+tasks.withType<PublishToMavenLocal> {
+
 }
